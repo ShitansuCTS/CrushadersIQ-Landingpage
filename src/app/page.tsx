991ee78dea499081b5688978264df34ce16c5dcc
Shitansu7205 <shitansukumargochhayat@gmail.com>
2026-08-20 16:01:13 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import HomeOne from "@/components/homes/home-1";
+import HomeTwo from "@/components/homes/home-2";
+
+
+
+export default function index() {
+  return (
+    // <HomeOne />
+    <HomeTwo />
+  )
+}
